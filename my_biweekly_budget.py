f44064fc374ae8108faf440875766789e3f451b2
Madeline Muse <redstaratlas@gmail.com>
2022-06-29 08:41:41 -0500

    continuing going line by line both understanding and making it my own

--- my_biweekly_budget.py
+++ my_biweekly_budget.py
@@ -40,6 +40,7 @@ class BiWeekly(object):
         self.investments = investments
         self.biweekly_allowance = biweekly_allowance
         self.actual_spent = actual_spent
+        self.add_expense = add_expense
         self.name = name
         self. prev_two_weeks = prev_two_weeks
 
@@ -51,5 +52,49 @@ class BiWeekly(object):
             print("$" + x, "of actual spent recorded")
         else:
             print("Invalid amount type, please only put the numbers without any signs")
+    
+    #How much of the Misc expenses were spent
+    def additional_expenses(self):
+        while not expense.isnumeric():
+            expense = input("Please add an expense amount, only the numeric value")
+
+        expense = float(expense)
+        self.add_expense += expense
+
+        while another_expense.lower() != "none":
+            another_expense = input("Please add another expense, or type 'none' if finished: ")
+            while not another_expense.isnumeric() or float(another_expense) < 0 or not another_expense.lower() == "none":
+                another_expense = input("Please input a valid number: ")
+
+            another_expense = float(ex)
+            self.add_expense += another_expense
+
+    def add_income(self):
+        amount = input("Please enter the amount: ")
+        while not amount.isnumeric() or float(amount) < 0:
+            amount = input("Invalid amount type, please only use digits and decimals. Income cannot be negative. Press 'Q' to quit or enter a valid amount: ")
+            if amount.lower() == "q":
+                return
+
+            self.biweekly_allowance += float(amount) < 0:
+            print(amount, "added to biweekly income")
+
+    def calc_current_leftover(self):
+        return self.prev_two_weeks.calc_leftover()
+
+    #calculates the amount of spending income
+    def calc_spending_income(self):
+        assests = self.biweekly_allowance + self.leftover
+        expenses = self.housing + self.utilities + self.groceries + self.add_expense
+        return assests - expenses
+
+    #calculates the leftover income to next month
+    def calc_leftover(self):
+        spending_income = self,calc_spending_income()
+
+        return spending_income - self.actual_spent
+
+        
+
 
     
