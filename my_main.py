66faabaa8e1a72a2b4fb9738ae22a32b644fb6d2
Madeline Muse <redstaratlas@gmail.com>
2022-07-01 21:06:27 -0500

    going to try to get this to work now

--- my_main.py
+++ my_main.py
@@ -1,5 +1,5 @@
-from my_annual_budget import Annual_Budget
-from my_biweekly_budget import import BiWeekly
+from annual_budget import Annual_Budget
+from biweekly_budget import import BiWeekly
 
 def new_budget():
     out_file = input("What is the name of your new budget?: ")
@@ -26,3 +26,150 @@ def new_budget():
 
     annual_budget = Annual_Budget(income, housing, groceries, utilities, gas, phone, loans,
                                 subscriptions,  num_months)
+
+    if not annual_budget.month_list:
+        print("Please create a month to begin")
+        annual_budget.create_month()
+
+    annual_budget.annual_menu()
+
+    months_created = len(annual_budget.month_list)
+
+    annual_list = [annual_budget, out_file]
+
+    return annual_list
+
+def continue_budget():
+    in_file = input("Which budget file would you like to continue: ")
+    if in_file[-4] != ".txt":
+        in_file += ".txt"
+
+    try:
+        budget_data = open(in_file, "r")
+
+    except FileNotFoundError:
+        print("File not found")
+
+        in_file = input("Try another file name, or enter 'q' to quit to main menu: ")
+        if in_file == 'q':
+            return
+        else:
+            if in_file[-4:] != ".txt":
+                in_file = in_file + ".txt"
+            try:
+                budget_data = open(in_file, "r")
+            except FileNotFoundError:
+                print("File not found again", '\n')
+                return
+
+    income = float(budget_data.readline().strip())
+    groceries = float(budget_data.readline().strip())
+    utilities = float(budget_data.readline().strip())
+    housing = float(budget_data.readline().strip())
+    gas = float(budget_data.readline().strip())
+    phone = float(budget_data.readline().strip())
+    loans = float(budget_data.readline().strip())    
+    subscriptions = float(budget_data.readline().strip())
+    num_month = float(budget_data.readline().strip())
+    num_created_months = int(budget_data.readline().strip())
+
+    annual_budget = Annual_Budget(income,groceries,utilities,housing, gas, phone, loans, 
+                                    subscriptions, num_month)
+
+
+    for i in range(num_created_months):
+        new_line = budget_data.readline().split("*")
+
+
+        if not annual_budget.month_list:
+            month = Month(new_line[0], float(new_line[1]), float(new_line[2]), float(new_line[3]),float(new_line[4]), float(new_line[5]), float(new_line[6]))
+
+        else:
+            prev_month = annual_budget.month_list[-1]
+
+            month = Month(new_line[0], float(new_line[1]), float(new_line[2]), float(new_line[3]),float(new_line[4]), float(new_line[5]), float(new_line[6]), prev_month)
+
+        annual_budget.month_list.append(month)
+
+
+    budget_data.close()
+
+
+
+    if not annual_budget.month_list:
+        print("Please create a month to begin")
+
+        annual_budget.create_month()
+
+    annual_budget.annual_menu()
+
+    annual_lst = [annual_budget,in_file]
+
+    return annual_lst
+
+def menu():
+    print("Please choose from the following options: ")
+    print("             ")   
+    print("-----------------------------------------")
+    print("1: Create a new budget")
+    print("2: Continue an existing budget")
+    print("-----------------------------------------")
+    print("             ")
+    pick = input("Enter: ")
+
+    while pick != "1" and pick != "2":
+        pick = input("Please enter a valid number: ")
+
+    return int(pick)
+
+def main():
+
+    pick = menu()
+
+    if pick == 1:
+        n_file = new_budget()
+    elif pick == 2:
+        n_file = continue_budget()
+
+    try:
+        new_budget_data = open(n_file[1], "w")
+
+    except Exception:
+        print("Something went wrong with exporting the data")
+
+
+    annual_budget = n_file[0]
+
+    income = str(annual_budget.income)
+    groceries = str(annual_budget.groceries)
+    utilities = str(annual_budget.utilities)
+    housing = str(annual_budget.housing)
+    num_months = str(annual_budget.num_months)
+    num_months_created = str(len(annual_budget.month_list))
+
+    new_budget_data.write(income)
+    new_budget_data.write("\n")
+    new_budget_data.write(groceries)
+    new_budget_data.write("\n")
+    new_budget_data.write(utilities)
+    new_budget_data.write("\n")
+    new_budget_data.write(housing)
+    new_budget_data.write("\n")
+    new_budget_data.write(num_months)
+    new_budget_data.write("\n")
+    new_budget_data.write(num_months_created)
+    new_budget_data.write("\n")
+
+    for i in annual_budget.month_list:
+        if i.prev_month != None:
+            prev_month_name = i.prev_month.name
+        else:
+            prev_month_name = "-1"
+
+        new_budget_data.write(i.name + "*" + str(i.housing)+ "*" + str(i.monthly_allowance) + "*" + str(i.groceries)+ "*" + str(i.utilities) + "*" + str(i.actual_spending) + "*" + str(i.add_expense)+ "*" + prev_month_name)
+        new_budget_data.write("\n")
+    new_budget_data.close()
+
+    print("Program ended.")
+
+main()
